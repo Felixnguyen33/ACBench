36101299e3a1485f246f8769930bff3618f2dac3
pprp <1115957667@qq.com>
2024-11-26 13:40:47 +0800

    Refactor the structure

---
+++ scripts/vllm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+# env vllm
+
+/mnt/sdb/dongpeijie/miniconda3/envs/vllm/bin/vllm serve NousResearch/Meta-Llama-3-8B-Instruct --dtype auto --api-key token-abc123
--- vllm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-# env vllm
-
-vllm serve NousResearch/Meta-Llama-3-8B-Instruct --dtype auto --api-key token-abc123
