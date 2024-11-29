1a7f886b52ac01c47fa69e9885911b040b6268c8
pprp <1115957667@qq.com>
2024-11-29 13:07:55 +0800

    Refactor it

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ def parse_requirements(fname='requirements.txt', with_version=True):
 
 if __name__ == '__main__':
     setup(
-        name='benchtools',
+        name='agentbench',
         version='0.1',
         description='library for benchtools',
         long_description=readme(),
