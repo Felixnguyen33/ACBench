98759ba2add93a7af6e379a4e7d2a5d8ef1e82b9
pprp <1115957667@qq.com>
2025-03-27 22:54:26 +0800

    Refactor to ACBench

--- setup.py
+++ setup.py
@@ -84,9 +84,9 @@ def parse_requirements(fname='requirements.txt', with_version=True):
 
 if __name__ == '__main__':
     setup(
-        name='agentbench',
+        name='acbench',
         version='0.1',
-        description='library for agentbench',
+        description='library for acbench',
         long_description=readme(),
         long_description_content_type='text/markdown',
         keywords='computer vision, model compression',
